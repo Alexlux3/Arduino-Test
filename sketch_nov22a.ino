eaf5af26968692da1fc3c22067954c5a8ec7fb16
Alex <calex0740@gmail.com>
2023-11-22 20:53:36 -0500

    Agregado proyecto de Arduino

---
+++ sketch_nov22a.ino
@@ -0,0 +1,15 @@
+//Programa para encender y apagar un Led
+
+const int LED=10;
+void setup() {
+
+}
+
+pinMode(LED, OUTPUT);
+void loop() {
+
+}
+digitalWrite(LED, HIGH);
+delay(1000);
+digitalWrite(LED, LOW);
+delay(1000);
